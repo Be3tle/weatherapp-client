6dcf4640d2e8e3e749f40926efdab8d90181c3e5
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-02-04 23:02:55 +0600

    auth provider added

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import Router from './Routes/routes';
+import { AuthContext } from './Provider/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <AuthContext>
+      <RouterProvider router={Router} />
+    </AuthContext>
   </React.StrictMode>
 );
